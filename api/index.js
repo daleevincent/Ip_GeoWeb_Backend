5e02e3198cbe1a43c850870a746a936244fd98d1
unknown <dalevincent94@gmail.com>
2026-03-03 19:02:30 +0800

    vercel.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
 import app from "../src/server.js";
-import { createServer } from "http";
 
-export default function handler(req, res) {
-  // Wrap Express app
-  const server = createServer(app);
-  server.emit("request", req, res);
-}
+export default app;
