8a13fd6ac9869d66e7717666b40d7bdb2034c614
daleevincent <you@example.com>
2026-03-03 20:26:50 +0800

    Revert "vercel.json"
    This reverts commit 5e02e3198cbe1a43c850870a746a936244fd98d1.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 import app from "../src/server.js";
+import { createServer } from "http";
 
-export default app;
+export default function handler(req, res) {
+  // Wrap Express app
+  const server = createServer(app);
+  server.emit("request", req, res);
+}
